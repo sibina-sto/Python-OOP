501a0d57fe8f1b4c6a08ec01266ad2151ec11cfc
piziamko <stoyanovasibina@gmail.com>
2021-04-10 21:55:10 +0300

    Create Player.py

---
+++ 04. Encapsulation/Exercise/03. Football Team Generator/Player.py
@@ -0,0 +1,63 @@
+class Player:
+    def __init__(self, name, endurance, sprint, dribble, passing, shooting):
+        self.__name = name
+        self.__endurance = endurance
+        self.__sprint = sprint
+        self.__dribble = dribble
+        self.__passing = passing
+        self.__shooting = shooting
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    @property
+    def endurance(self):
+        return self.__endurance
+
+    @endurance.setter
+    def endurance(self, new_endurance):
+        self.endurance = new_endurance
+
+    @property
+    def sprint(self):
+        return self.__sprint
+
+    @sprint.setter
+    def sprint(self, new_sprint):
+        self.__sprint = new_sprint
+
+    @property
+    def dribble(self):
+        return self.__dribble
+
+    @dribble.setter
+    def dribble(self, new_dribble):
+        self.__dribble = new_dribble
+
+    @property
+    def passing(self):
+        return self.__passing
+
+    @passing.setter
+    def passing(self, new_passing):
+        self.__passing = new_passing
+
+    @property
+    def shooting(self):
+        return self.__shooting
+
+    @shooting.setter
+    def shooting(self, new_shooting):
+        self.__shooting = new_shooting
+
+    def __str__(self):
+        return f"Player: {self.__name}\n" \
+               f"Endurance: {self.__endurance}\n" \
+               f"Sprint: {self.__sprint}\n" \
+               f"Dribble: {self.__dribble}\n" \
+               f"Passing: {self.__passing}\n"\
+               f"Shooting: {self.__shooting}\n"
