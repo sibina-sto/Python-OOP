53b352b182a6c2c5099d1067e237af0b73fa10e8
piziamko <stoyanovasibina@gmail.com>
2021-04-14 23:34:47 +0300

    Create Stack-of-Strings.py

---
+++ 05. Inheritance/Lab/Stack-of-Strings.py
@@ -0,0 +1,42 @@
+class Stack:
+    def __init__(self):
+        self.data = []
+
+    def push(self, item):
+        if isinstance(item, str):
+            self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+
+    def peek(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return False if self.data else True
+
+
+    def __str__(self):
+        return f"[{', '.join(self.data[::-1])}]"
+
+
+# test zero
+import unittest
+
+
+class StackTests(unittest.TestCase):
+    def test_zero(self):
+        stack = Stack()
+        stack.push("apple")
+        stack.push("carrot")
+        self.assertEqual(str(stack), '[carrot, apple]')
+        self.assertEqual(stack.pop(), 'carrot')
+        self.assertEqual(stack.peek(), 'apple')
+        stack.push("cucumber")
+        self.assertEqual(str(stack), '[cucumber, apple]')
+        self.assertEqual(stack.is_empty(), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
