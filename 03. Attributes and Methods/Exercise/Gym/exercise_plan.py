b1ed2c6ac281b3f2a1d1313327b8947e65775c94
piziamko <stoyanovasibina@gmail.com>
2021-04-14 17:11:12 +0300

    Create exercise_plan.py

---
+++ 03. Attributes and Methods/Exercise/Gym/exercise_plan.py
@@ -0,0 +1,20 @@
+class ExercisePlan:
+    id_ = 0
+
+    def __init__(self, trainer_id, equipment_id, duration):
+        self.trainer_id = trainer_id
+        self.equipment_id = equipment_id
+        self.duration = duration
+        self.id = 1
+
+    @classmethod
+    def from_hours(cls, trainer_id: int, equipment_id: int, hours: int):
+        return cls(trainer_id, equipment_id, hours*60)
+
+    @staticmethod
+    def get_next_id():
+        ExercisePlan.id_ += 1
+        return ExercisePlan.id_
+
+    def __repr__(self):
+        return f"Plan <{self.id}> with duration {self.duration} minutes"
