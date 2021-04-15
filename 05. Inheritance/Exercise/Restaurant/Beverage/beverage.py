4491ed52eecc6032190abee570eddd0d4634ef35
piziamko <stoyanovasibina@gmail.com>
2021-04-15 19:20:00 +0300

    Create beverage.py

---
+++ 05. Inheritance/Exercise/Restaurant/Beverage/beverage.py
@@ -0,0 +1,11 @@
+from product import Product
+
+
+class Beverage(Product):
+    def __init__(self, name: str, price: float, milliliters: float):
+        super().__init__(name, price)
+        self._milliliters = milliliters
+
+    @property
+    def milliliters(self):
+        return self._milliliters
