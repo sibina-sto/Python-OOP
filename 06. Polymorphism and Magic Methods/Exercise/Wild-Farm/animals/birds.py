d768b5350cb54695262bacbfec2c97284754b347
piziamko <stoyanovasibina@gmail.com>
2021-05-09 23:33:30 +0300

    Create birds.py

---
+++ 06. Polymorphism and Magic Methods/Exercise/Wild-Farm/animals/birds.py
@@ -0,0 +1,33 @@
+
+from Animals.animals.animal import Bird
+from Animals.food import Meat, Vegetable, Fruit, Seed
+
+
+class Owl(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    def make_sound(self):
+        return "Hoot Hoot"
+
+    def feed(self, food):
+        if isinstance(food, Meat):
+            self.weight += 0.25 * food.quantity
+            self.food_eaten += food.quantity
+            return
+        return f"{self.__class__.__name__} does not eat {food.__class__.__name__}!"
+
+
+class Hen(Bird):
+    def __init__(self, name, weight, wing_size):
+        super().__init__(name, weight, wing_size)
+
+    def make_sound(self):
+        return "Cluck"
+
+    def feed(self, food):
+        if isinstance(food, Meat) or isinstance(food, Vegetable) or isinstance(food, Fruit) or isinstance(food, Seed):
+            self.weight += food.quantity * 0.35
+            self.food_eaten += food.quantity
+            return
+        return f'{self.__class__.__name__} does not eat {food.__class__.__name__}!'
