a2cf261bf4c91fd0bcb0031805f2e3ae8bfcfda9
piziamko <stoyanovasibina@gmail.com>
2021-05-09 17:50:30 +0300

    Create Animal.py

---
+++ 07. Solid/Lab/02_OCP/Animal.py
@@ -0,0 +1,20 @@
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+    def get_species(self):
+        return self.species
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        if animal.species == 'cat':
+            print('meow')
+        elif animal.species == 'dog':
+            print('woof-woof')
+
+
+animals = [Animal('cat'), Animal('dog')]
+animal_sound(animals)
+
+# animals = [Animal('cat'), Animal('dog'), Animal('chicken')]
