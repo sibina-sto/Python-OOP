6dbec0371be287e993839236a4a03c8969dfb35f
piziamko <stoyanovasibina@gmail.com>
2021-07-01 02:31:38 +0300

    Create 02. Time.py

---
+++ 02. Classes and Objects/Exercise/02. Time.py
@@ -0,0 +1,36 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        return f"{self.hours:0>2d}:{self.minutes:0>2d}:{self.seconds:0>2d}"
+
+    def next_second(self):
+        if self.seconds == Time.max_seconds:
+            self.seconds = 0
+            if self.minutes == Time.max_minutes:
+                self.minutes = 0
+                if self.hours == Time.max_hours:
+                    self.hours = 0
+                else:
+                    self.hours += 1
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+        return f"{self.hours:0>2d}:{self.minutes:0>2d}:{self.seconds:0>2d}"
+
+
+time = Time(1, 20, 30)
+print(time.next_second())
