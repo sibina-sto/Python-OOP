02f433410cae73d895155bcd2303d174a83660e3
piziamko <stoyanovasibina@gmail.com>
2021-06-30 12:55:17 +0300

    Delete 01. Defining Classes directory

--- 01. Defining Classes/Exercise/OOP-project-Pokemon-Battle/__init__.py
+++
@@ -1,1 +0,0 @@
-from .pokemon import Pokemon
--- 01. Defining Classes/Exercise/OOP-project-Spoopify/song.py
+++
@@ -1,9 +0,0 @@
-class Song:
-    def __init__(self, name, length, is_single=bool):
-        self.name = name
-        self.length = float(length)
-        self.is_single = is_single
-
-
-    def get_info(self):
-        return f"{self.name} - {self.length}"
