b75c44b8d08a6723f9701845d4e0f88afab8951d
piziamko <stoyanovasibina@gmail.com>
2021-07-04 14:16:29 +0300

    Create 02. Mammal.py

---
+++ 04. Encapsulation/Lab/02. Mammal.py
@@ -0,0 +1,16 @@
+class Mammal:
+    __kingdom = 'animals'
+
+    def __init__(self, name, type_, sound):
+        self.type = type_
+        self.sound = sound
+        self.name = name
+
+    def make_sound(self):
+        return f"{self.name} makes {self.sound}"
+
+    def get_kingdom(self):
+        return Mammal.__kingdom
+
+    def info(self):
+        return f"{self.name} is of type {self.type}"
