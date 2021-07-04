483c7bd7fea0dba21e64f7a3a5108f4dfe85183c
piziamko <stoyanovasibina@gmail.com>
2021-07-05 00:11:00 +0300

    Create tea.py

---
+++ 04. Encapsulation/Exercise/04. Restaurant/project/beverage/tea.py
@@ -0,0 +1,6 @@
+# from inheritance_exercises.restaurant.project.beverage.hot_beverage import HotBeverage
+from project.beverage.hot_beverage import HotBeverage
+
+
+class Tea(HotBeverage):
+    pass
