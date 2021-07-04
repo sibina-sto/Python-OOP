d52e71a039940725d6143743f5926ad528514ebd
piziamko <stoyanovasibina@gmail.com>
2021-07-04 23:06:09 +0300

    Update 01. Photo Album.py

--- 05. Static and Class Methods/Exercise/01. Photo Album.py
+++ 05. Static and Class Methods/Exercise/01. Photo Album.py
@@ -1,27 +1,38 @@
 class PhotoAlbum:
     def __init__(self, pages):
         self.pages = pages
-        self.photos = [[] for _ in range(pages)]
+        self.photos = []
+        for _ in range(pages):
+            self.photos.append([])
 
     @classmethod
-    def from_photos_count(cls, photos_count: int):
+    def from_photos_count(cls, photos_count):
         return cls(photos_count // 4)
 
     def add_photo(self, label):
-        for page in range(len(self.photos)):
-            if len(self.photos[page]) < 4:
-                self.photos[page].append(label)
-                return f"{label} photo added successfully on page {page + 1} slot {len(self.photos[page])}"
+        if not len(self.photos[-1]) == 4:
+            for page in self.photos:
+                if len(page) != 4:
+                    page.append(label)
+                    break
+            return f"{label} photo added successfully on" \
+                   f" page {self.photos.index(page) + 1} slot {page.index(label) + 1}"
         return "No more free spots"
 
-    @staticmethod
-    def print_line_of_slashes():
-        return '-----------'
-
     def display(self):
-        result = ''
-        for page in range(self.pages):
-            result += f'{self.print_line_of_slashes()}\n'
-            result += f'{" ".join(["[]" for _ in range(len(self.photos[page]))])}\n'
-        result += f'{self.print_line_of_slashes()}\n'
-        return result
+        res = "-----------\n"
+        for p in self.photos:
+            res += (''.join(["[] " for s in p])).strip()
+            res += "\n-----------\n"
+        return res
+
+
+album = PhotoAlbum(2)
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+print(album.display())
