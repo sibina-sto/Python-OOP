d96c7645fdc77d3ac8f33a4847dc0d61c85c8cfa
piziamko <stoyanovasibina@gmail.com>
2021-07-05 00:14:59 +0300

    Create cake.py

---
+++ 04. Encapsulation/Exercise/04. Restaurant/project/food/cake.py
@@ -0,0 +1,11 @@
+# from inheritance_exercises.restaurant.project.food.dessert import Dessert
+from project.food.dessert import Dessert
+
+
+class Cake(Dessert):
+    GRAMS = 250
+    CALORIES = 1000
+    PRICE = 5
+
+    def __init__(self, name):
+        super().__init__(name, Cake.PRICE, Cake.GRAMS, Cake.CALORIES)
