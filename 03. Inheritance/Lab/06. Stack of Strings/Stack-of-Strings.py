2ba82114f21c6864def5073411487977ee251a7f
piziamko <stoyanovasibina@gmail.com>
2021-07-09 09:38:28 +0300

    Update Stack-of-Strings.py

--- 03. Inheritance/Lab/06. Stack of Strings/Stack-of-Strings.py
+++ 03. Inheritance/Lab/06. Stack of Strings/Stack-of-Strings.py
@@ -2,37 +2,17 @@ class Stack:
     def __init__(self):
         self.data = []
 
+    def __repr__(self):
+        return f'[{", ".join([str(c) for c in reversed(self.data)])}]'
+
     def push(self, item):
-        if isinstance(item, str):
-            self.data.append(item)
+        self.data.append(item)
 
     def pop(self):
         return self.data.pop()
 
-    def peek(self):
+    def top(self):
         return self.data[-1]
 
     def is_empty(self):
-        return False if self.data else True
-
-    def __str__(self):
-        return f"[{', '.join(self.data[::-1])}]"
-
-# test zero
-import unittest
-
-class StackTests(unittest.TestCase):
-    def test_zero(self):
-        stack = Stack()
-        stack.push("apple")
-        stack.push("carrot")
-        self.assertEqual(str(stack), '[carrot, apple]')
-        self.assertEqual(stack.pop(), 'carrot')
-        self.assertEqual(stack.peek(), 'apple')
-        stack.push("cucumber")
-        self.assertEqual(str(stack), '[cucumber, apple]')
-        self.assertEqual(stack.is_empty(), False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        return len(self.data) == 0
