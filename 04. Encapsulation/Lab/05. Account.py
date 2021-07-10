e4da20ad2832e245c4b0ec6406b0dd3ed4fc8feb
piziamko <stoyanovasibina@gmail.com>
2021-07-10 22:11:40 +0300

    Update 05. Account.py

--- 04. Encapsulation/Lab/05. Account.py
+++ 04. Encapsulation/Lab/05. Account.py
@@ -1,27 +1,18 @@
-class Account:
-    def __init__(self, id_, balance, pin):
-        self.__pin = pin
-        self.balance = balance
-        self.__id = id_
+# Ines
 
-    def __validate_pin(self, pin):
-        return self.__pin == pin
+class Account:
+    def __init__(self, id, balance, pin):
+        self.__id = id
+        self.balance = balance
+        self.__pin = pin
 
     def get_id(self, pin):
-        if self.__validate_pin(pin):
+        if self.__pin == pin:
             return self.__id
         return 'Wrong pin'
 
     def change_pin(self, old_pin, new_pin):
-        if self.__validate_pin(old_pin):
+        if old_pin == self.__pin:
             self.__pin = new_pin
             return 'Pin changed'
-        return "Wrong pin"
-
-
-account = Account(8827312, 100, 3421)
-print(account.get_id(1111))
-print(account.get_id(3421))
-print(account.balance)
-print(account.change_pin(2212, 4321))
-print(account.change_pin(3421, 1234))
+        return 'Wrong pin'
