7550b816e7e3a62a709527f9616652cd00c46e98
piziamko <stoyanovasibina@gmail.com>
2021-07-10 22:09:36 +0300

    Update 03. Profile.py

--- 04. Encapsulation/Lab/03. Profile.py
+++ 04. Encapsulation/Lab/03. Profile.py
@@ -1,5 +1,4 @@
 class Profile:
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -24,13 +23,12 @@ class Profile:
         if self.is_len_valid(value) and self.is_digit(value) and self.is_upper(value):
             self.__pasword = value
             return
-        raise ValueError(
-            f"The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        raise ValueError(f"The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
 
     def is_len_valid(self, password):
         if len(password) > 7:
             return True
-        False
+        return False
 
     def is_digit(self, password):
         nums = [num for num in password if num.isdigit()]
@@ -42,61 +40,3 @@ class Profile:
 
     def __str__(self):
         return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.__pasword)}'
-
-
-correct_profile = Profile("Username", "Passw0rd")
-print(correct_profile)
-
-#
-import re
-
-
-class Profile:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    @property
-    def username(self):
-        return self.__username
-
-    @username.setter
-    def username(self, value):
-        if len(value) < 5 or len(value) > 15:
-            raise ValueError('The username must be between 5 and 15 characters.')
-        self.__username = value
-
-    @property
-    def password(self):
-        return self.__password
-
-    @password.setter
-    def password(self, value):
-        if re.findall('[0-9]', value) and len(value) >= 8 and re.match('[A-Z]', value):
-            self.__password = value
-            return
-        raise ValueError('The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.')
-
-    def __str__(self):
-        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.password)}'
-
-
-import unittest
-
-class Tests(unittest.TestCase):
-    def test_invalid_password(self):
-        with self.assertRaises(ValueError) as ve:
-            self.profile = Profile('My_username', 'My-password')
-        self.assertEqual(str(ve.exception), "The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
-
-    def test_invalid_username(self):
-        with self.assertRaises(ValueError) as ve:
-            self.profile = Profile('Too_long_username', 'Any')
-        self.assertEqual(str(ve.exception), "The username must be between 5 and 15 characters.")
-
-    def test_correct_profile(self):
-        self.profile = Profile("Username", "Passw0rd")
-        self.assertEqual(str(self.profile), 'You have a profile with username: "Username" and password: ********')
-
-if __name__ == "__main__":
-    unittest.main()
