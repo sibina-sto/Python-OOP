393ce5d0698260e811a7293dd127d4c573a4aa42
piziamko <stoyanovasibina@gmail.com>
2021-07-11 22:37:32 +0300

    Update cheetah.py

--- 04. Encapsulation/Exercise/01. Wild Cat Zoo/project/cheetah.py
+++ 04. Encapsulation/Exercise/01. Wild Cat Zoo/project/cheetah.py
@@ -1,6 +1,8 @@
+
 from project.animal import Animal
 
 
 class Cheetah(Animal):
-    def get_needs(self):
-        return 60
+
+    def __init__(self, name, gender, age):
+        super().__init__(name, gender, age, 60)
