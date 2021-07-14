318512c65360cdcb2b21abd3039bfb7e6589b93f
piziamko <stoyanovasibina@gmail.com>
2021-07-14 22:10:30 +0300

    Create 01. Vehicles.py

---
+++ 06. Polymorphism and Magic Methods/Exercise/01. Vehicles.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    consumption_increase = 0.9
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        fuel = distance * (self.fuel_consumption + Car.consumption_increase)
+        if self.fuel_quantity - fuel < 0:
+            return
+
+        self.fuel_quantity -= fuel
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    consumption_increase = 1.6
+
+    def __init__(self, fuel_quantity, fuel_consumption):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    def drive(self, distance):
+        fuel = distance * (self.fuel_consumption + Truck.consumption_increase)
+        if self.fuel_quantity - fuel < 0:
+            return
+
+        self.fuel_quantity -= fuel
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
