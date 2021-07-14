284fe378e5af1339f073922dca0fc49719a0a47b
piziamko <stoyanovasibina@gmail.com>
2021-07-14 21:11:26 +0300

    Delete 06. Polymorphism and Magic Methods/Exercise directory

--- 06. Polymorphism and Magic Methods/Exercise/Animals/kitten.py
+++
@@ -1,9 +0,0 @@
-from Animals.cat import Cat
-
-
-class Kitten(Cat):
-    def __init__(self, name, age):
-        super().__init__(name, age, gender="Female")
-
-    def make_sound(self):
-        return "Meow"
