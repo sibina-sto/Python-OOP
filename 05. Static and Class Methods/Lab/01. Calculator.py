c0751afc587cd6a4f0ccca825099e11b90db6981
piziamko <stoyanovasibina@gmail.com>
2021-07-15 16:55:18 +0300

    Update 01. Calculator.py

--- 05. Static and Class Methods/Lab/01. Calculator.py
+++ 05. Static and Class Methods/Lab/01. Calculator.py
@@ -1,1 +1,20 @@
+# Ines 
 
+from functools import reduce
+
+class Calculator:
+    @staticmethod
+    def add(*args):
+        return sum(args)
+
+    @staticmethod
+    def multiply(*args):
+        return reduce(lambda x, y: x * y, args)
+
+    @staticmethod
+    def divide(*args):
+        return reduce(lambda x, y: x / y, args)
+
+    @staticmethod
+    def subtract(*args):
+        return reduce(lambda x, y: x - y, args)
