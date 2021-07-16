eb6dfa43e174fcc5b763d92c7a7fad870b1ab424
piziamko <stoyanovasibina@gmail.com>
2021-07-16 22:04:35 +0300

    Update Hotel.py

--- 05. Static and Class Methods/Lab/04. Hotel Rooms/project/Hotel.py
+++ 05. Static and Class Methods/Lab/04. Hotel Rooms/project/Hotel.py
@@ -6,40 +6,25 @@ class Hotel:
 
     @classmethod
     def from_stars(cls, stars_count):
-        return cls(f'{stars_count} stars Hotel')
-
-    @staticmethod
-    def find_room(room_number, list_of_rooms):
-        return [index for index in range(len(list_of_rooms)) if list_of_rooms[index].number == room_number]
+        return cls(f"{stars_count} stars Hotel")
 
     def add_room(self, room):
         self.rooms.append(room)
 
     def take_room(self, room_number, people):
-        find = self.find_room(room_number, self.rooms)
-        if find:
-            actual_index = find[0]
-            self.rooms[actual_index].take_room(people)
+        room = [r for r in self.rooms if r.number == room_number][0]
+        result = room.take_room(people)
+        if not result:
+            self.guests += people
 
     def free_room(self, room_number):
-        find = self.find_room(room_number, self.rooms)
-        if find:
-            actual_index = find[0]
-            self.rooms[actual_index].free_room()
+        room = [r for r in self.rooms if r.number == room_number][0]
+        self.guests -= room.guests
+        room.free_room()
 
     def status(self):
-        result = ''
-        free = ", ".join([str(room.number) for room in self.rooms if not room.is_taken])
-        taken = ", ".join([str(room.number) for room in self.rooms if room.is_taken])
-        result += f'Hotel {self.name} has {self.guests} total guests\n'
-        result += f'Free rooms: {free}\n'
-        result += f'Taken rooms: {taken}'
-        return result
-
-    @property
-    def guests(self):
-        return sum([r.guests for r in self.rooms])
-
-    @guests.setter
-    def guests(self, value):
-        self.__guests = value
+        numbers_free_rooms = [r.number for r in self.rooms if not r.is_taken]
+        numbers_taken_rooms = [r.number for r in self.rooms if r.is_taken]
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join(map(str, numbers_free_rooms))}\n" \
+               f"Taken rooms: {', '.join(map(str, numbers_taken_rooms))}"
