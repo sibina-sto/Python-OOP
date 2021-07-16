b092b48b6b835ecb031a0800eb0061063bbe0581
piziamko <stoyanovasibina@gmail.com>
2021-07-16 22:09:42 +0300

    Update 03. Image Area.py

--- 06. Polymorphism and Magic Methods/Lab/03. Image Area.py
+++ 06. Polymorphism and Magic Methods/Lab/03. Image Area.py
@@ -6,18 +6,18 @@ class ImageArea:
     def get_area(self):
         return self.width * self.height
 
-    def __eq__(self, other):
-        return self.get_area() == other.get_area()
-
     def __gt__(self, other):
-        return self.get_area() >= other.get_area()
+        return self.get_area() > other.get_area()
 
     def __ge__(self, other):
         return self.get_area() >= other.get_area()
+
+    def __eq__(self, other):
+        return self.get_area() == other.get_area()
 
 
 a1 = ImageArea(7, 10)
 a2 = ImageArea(35, 2)
 a3 = ImageArea(8, 9)
-print(a1 == a2)
-print(a1 != a3)
+print(a1 != a2)
+print(a1 >= a3)
