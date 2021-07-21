b491056f850667c23f853b6a74454a5ba78155fa
piziamko <stoyanovasibina@gmail.com>
2021-07-21 14:17:26 +0300

    Update saltwater_fish.py

--- 0. Python-OOP - Exam/10_Apr_2021/project/fish/saltwater_fish.py
+++ 0. Python-OOP - Exam/10_Apr_2021/project/fish/saltwater_fish.py
@@ -2,10 +2,12 @@ from project.fish.base_fish import BaseFish
 
 
 class SaltwaterFish(BaseFish):
-    default_size = 5
+    AQUARIUM_TYPE = "SaltwaterAquarium"
+    INITIAL_SIZE = 5
 
     def __init__(self, name: str, species: str, price: float):
-        super().__init__(name, species, self.default_size, price)
+        super().__init__(name, species, self.INITIAL_SIZE, price)
+        self.aquarium = self.AQUARIUM_TYPE
 
     def eat(self):
         self.size += 2
