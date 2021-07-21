68a014c85b0591398e3be84854c737e181fcce61
piziamko <stoyanovasibina@gmail.com>
2021-07-21 14:16:46 +0300

    Update freshwater_fish.py

--- 0. Python-OOP - Exam/10_Apr_2021/project/fish/freshwater_fish.py
+++ 0. Python-OOP - Exam/10_Apr_2021/project/fish/freshwater_fish.py
@@ -2,10 +2,12 @@ from project.fish.base_fish import BaseFish
 
 
 class FreshwaterFish(BaseFish):
-    default_size = 3
+    AQUARIUM_TYPE = "FreshwaterAquarium"
+    INITIAL_SIZE = 3
 
     def __init__(self, name: str, species: str, price: float):
-        super().__init__(name, species, self.default_size, price)
+        super().__init__(name, species, self.INITIAL_SIZE, price)
+        self.aquarium = self.AQUARIUM_TYPE
 
     def eat(self):
         self.size += 3
