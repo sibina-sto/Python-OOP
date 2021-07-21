1280851149dce6ed9f78272ba770ff775e373bca
piziamko <stoyanovasibina@gmail.com>
2021-07-21 14:11:16 +0300

    Update base_aquarium.py

--- 0. Python-OOP - Exam/10_Apr_2021/project/aquarium/base_aquarium.py
+++ 0. Python-OOP - Exam/10_Apr_2021/project/aquarium/base_aquarium.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
+
 class BaseAquarium(ABC):
+    @abstractmethod
     def __init__(self, name: str, capacity: int):
         self.name = name
         self.capacity = capacity
@@ -10,26 +12,26 @@ class BaseAquarium(ABC):
     @property
     def name(self):
         return self.__name
-    
+
     @name.setter
     def name(self, value):
-        if not value:
+        if value == "":
             raise ValueError("Aquarium name cannot be an empty string.")
         self.__name = value
 
-    @property
-    def avail_capacity(self):
-        return self.capacity - len(self.fish)
-
     def calculate_comfort(self):
-        return sum([deco.comfort for deco in self.decorations])
+        comfort_sum = sum([d.comfort for d in self.decorations])
+        return comfort_sum
 
-    @abstractmethod
     def add_fish(self, fish):
-        pass
+        if self.capacity < len(self.fish) + 1:
+            return "Not enough capacity."
+        self.fish.append(fish)
+        return f"Successfully added {fish.__class__.__name__} to {self.name}."
 
     def remove_fish(self, fish):
-        self.fish.remove(fish)
+        if fish in self.fish:
+            self.fish.remove(fish)
 
     def add_decoration(self, decoration):
         self.decorations.append(decoration)
@@ -38,13 +40,15 @@ class BaseAquarium(ABC):
         for fish in self.fish:
             fish.eat()
 
-    def get_fish_names(self):
-        result = [fish.name for fish in self.fish]
-        if result:
-            return " ".join(result)
-        return "none"
-
     def __str__(self):
-        return f'{self.name}:\nFish: {self.get_fish_names()}\n'\
-               f"Decorations: {len(self.decorations)}\n"\
-               f"Comfort: {self.calculate_comfort()}"
+        if len(self.fish) == 0:
+            fish_result = "none"
+        else:
+            fish_result = " ".join([f.name for f in self.fish])
+
+        result = f"{self.name}\n" \
+                 f"Fish: {fish_result}\n" \
+                 f"Decorations: {len(self.decorations)}\n" \
+                 f"Comfort: {self.calculate_comfort()}"
+
+        return result
