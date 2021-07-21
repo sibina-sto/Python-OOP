c8e337015a79c353a4a8f58aa914514180065ff7
piziamko <stoyanovasibina@gmail.com>
2021-07-21 14:15:15 +0300

    Update plant.py

--- 0. Python-OOP - Exam/10_Apr_2021/project/decoration/plant.py
+++ 0. Python-OOP - Exam/10_Apr_2021/project/decoration/plant.py
@@ -1,8 +1,9 @@
 from project.decoration.base_decoration import BaseDecoration
 
+
 class Plant(BaseDecoration):
-    default_comfort = 5
-    default_price = 10.0
+    COMFORT = 5
+    PRICE = 10.0
 
     def __init__(self):
-        super().__init__(self.default_comfort, self.default_price)
+        super().__init__(self.COMFORT, self.PRICE)
