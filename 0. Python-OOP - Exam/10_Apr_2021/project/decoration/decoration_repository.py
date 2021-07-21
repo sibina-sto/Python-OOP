ab13a45294765bcbaa50c2a64be61a54481f06bb
piziamko <stoyanovasibina@gmail.com>
2021-07-21 14:14:00 +0300

    Update decoration_repository.py

--- 0. Python-OOP - Exam/10_Apr_2021/project/decoration/decoration_repository.py
+++ 0. Python-OOP - Exam/10_Apr_2021/project/decoration/decoration_repository.py
@@ -1,23 +1,20 @@
 class DecorationRepository:
     def __init__(self):
+        # empty list that will contain all decorations (objects)
         self.decorations = []
-
-    @staticmethod
-    def get_obj_by_type(objects, obj_type):
-        result = [obj for obj in objects if obj.__class__.__name__ == obj_type]
-        return result
 
     def add(self, decoration):
         self.decorations.append(decoration)
 
     def remove(self, decoration):
-        if decoration not in self.decorations:
-            return False
-        self.decorations.remove(decoration)
-        return True
+        if decoration in self.decorations:
+            self.decorations.remove(decoration)
+            return True
+        return False
 
     def find_by_type(self, decoration_type: str):
-        decorations = self.get_obj_by_type(self.decorations,decoration_type)
-        if decorations:
-            return decorations[0]
-        return "None"
+        try:
+            decoration = [d for d in self.decorations if d.__class__.__name__ == decoration_type][0]
+            return decoration
+        except IndexError:
+            return "None"
