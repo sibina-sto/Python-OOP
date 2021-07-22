7905d88c2bf2861f6011fcb8cacab0a1454b18c8
piziamko <stoyanovasibina@gmail.com>
2021-07-22 19:08:45 +0300

    Delete 0. Python-OOP - Exam/10_Apr_2021 directory

--- 0. Python-OOP - Exam/10_Apr_2021/project/aquarium/freshwater_aquarium.py
+++
@@ -1,8 +0,0 @@
-from project.aquarium.base_aquarium import BaseAquarium
-
-
-class FreshwaterAquarium(BaseAquarium):
-    CAPACITY = 50
-
-    def __init__(self, name: str):
-        super().__init__(name, self.CAPACITY)
