a9b0c72d74166c5be48e026812d66009688b9c96
piziamko <stoyanovasibina@gmail.com>
2021-07-22 19:27:01 +0300

    Delete 0. Python-OOP - Exam/project/hardware directory

--- 0. Python-OOP - Exam/project/hardware/heavy_hardware.py
+++
@@ -1,6 +0,0 @@
-from project.hardware.hardware import Hardware
-
-
-class HeavyHardware(Hardware):
-    def __init__(self, name, capacity, memory):
-        super().__init__(name, "Heavy", int(2*capacity), int(0.75 * memory))
