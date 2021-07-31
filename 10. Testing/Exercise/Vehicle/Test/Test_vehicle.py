eaeace1a64977cd32cd8f0f2cee8ae6ec7473d4a
piziamko <stoyanovasibina@gmail.com>
2021-07-31 08:53:02 +0300

    Update Test_vehicle.py

--- 10. Testing/Exercise/Vehicle/Test/Test_vehicle.py
+++ 10. Testing/Exercise/Vehicle/Test/Test_vehicle.py
@@ -1,41 +1,47 @@
-from unittest import TestCase, main
+import unittest
 
-from vehicle import Vehicle
+from project.vehicle import Vehicle
 
 
-class TestVehicle(TestCase):
+class VehicleTests(unittest.TestCase):
+
     def setUp(self):
-        self.vehicle = Vehicle(50, 200)
+        self.vehicle = Vehicle(50, 100)
 
-    def test_vehicle_init__expect_to_initialized(self):
+    def test_vehicle_init__expect_initialization(self):
         self.assertEqual(50, self.vehicle.fuel)
-        self.assertEqual(200, self.vehicle.horse_power)
         self.assertEqual(50, self.vehicle.capacity)
-        self.assertEqual(self.vehicle.fuel_consumption, Vehicle.DEFAULT_FUEL_CONSUMPTION)
+        self.assertEqual(100, self.vehicle.horse_power)
+        self.assertEqual(1.25, self.vehicle.fuel_consumption)
 
-    def test_vehicle_drive_when_fuel_less_than_fuel_needed_expect_exception(self):
-        with self.assertRaises(Exception) as context:
-            self.vehicle.drive(50)
-        self.assertEqual("Not enough fuel", str(context.exception))
+    def test_vehicle_drive__if_not_enough_fuel__expect_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.vehicle.drive(100)
+        self.assertEqual("Not enough fuel", str(ex.exception))
 
-    def test_vehicle_drive_when_fuel_greater_than_fuel_needed_expect_fuel_decreases(self):
-        self.vehicle.drive(30)
-        self.assertEqual(12.5, self.vehicle.fuel)
+    def test_vehicle_drive__if_enough_fuel__execute(self):
+        self.vehicle.drive(20)
+        self.assertEqual(25, self.vehicle.fuel)
 
-    def test_vehicle_refuel_when_fuel_less_than_fuel_needed_expect_exception(self):
-        with self.assertRaises(Exception) as context:
-            self.vehicle.drive(30)
-            self.vehicle.refuel(50)
-        self.assertEqual("Too much fuel", str(context.exception))
+    def test_vehicle_refuel__if_over_capacity__expect_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.vehicle.refuel(25)
+        self.assertEqual("Too much fuel", str(ex.exception))
 
-    def test_vehicle_refuel_when_fuel_greater_than_fuel_needed_expect_fuel_increases(self):
-        self.vehicle.drive(30)
-        self.vehicle.refuel(17.5)
-        self.assertEqual(30, self.vehicle.fuel)
+    def test_vehicle_refuel__if_within_capacity__execute(self):
+        self.vehicle.drive(20)
+        self.vehicle.refuel(10)
+        self.assertEqual(35, self.vehicle.fuel)
 
-    def test_vehicle__str__expect_msg(self):
-        self.assertEqual('The vehicle has 200 horse power with 50 fuel left and 1.25 fuel consumption', self.vehicle.__str__())
+    def test_vehicle_str__to_return_str_message(self):
+        expected_msg = f"The vehicle has {self.vehicle.horse_power} " \
+               f"horse power with {self.vehicle.fuel} fuel left and {self.vehicle.fuel_consumption} fuel consumption"
+        actual_msg = self.vehicle.__str__()
+        self.assertEqual(expected_msg, actual_msg)
+
+    def test_vehicle_cls_attr__expect_correct_values(self):
+        self.assertEqual(1.25, self.vehicle.DEFAULT_FUEL_CONSUMPTION)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    unittest.main()
