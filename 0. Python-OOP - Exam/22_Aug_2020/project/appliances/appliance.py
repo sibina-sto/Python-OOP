73dbcd1026faf040ec6e57024ab58a3e3fb94575
piziamko <stoyanovasibina@gmail.com>
2021-08-14 14:42:21 +0300

    Add files via upload

---
+++ 0. Python-OOP - Exam/22_Aug_2020/project/appliances/appliance.py
@@ -0,0 +1,9 @@
+class Appliance:
+    days_in_month = 30
+
+    def __init__(self, cost: float):
+        self.cost = cost
+
+    def get_monthly_expense(self):
+        cost_for_month = self.cost * self.days_in_month
+        return cost_for_month
