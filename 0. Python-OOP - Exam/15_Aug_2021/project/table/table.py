228fc029cf5068109be462c8304a22b8429bbb6d
piziamko <stoyanovasibina@gmail.com>
2021-08-16 09:07:47 +0300

    Add files via upload

---
+++ 0. Python-OOP - Exam/15_Aug_2021/project/table/table.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+
+from project.baked_food.baked_food import BakedFood
+from project.drink.drink import Drink
+
+
+class Table(ABC):
+    @abstractmethod
+    def __init__(self, table_number: int, capacity: int):
+        self.table_number = table_number
+        self.capacity = capacity
+        self.food_orders = []
+        self.drink_orders = []
+        self.number_of_people = 0
+        self.is_reserved = False
+
+    @property
+    def capacity(self):
+        return self.__capacity
+
+    @capacity.setter
+    def capacity(self, value):
+        if value <= 0:
+            raise ValueError("Capacity has to be greater than 0!")
+        self.__capacity = value
+
+    def reserve(self, number_of_people: int):
+        self.number_of_people = number_of_people
+        self.is_reserved = True
+
+    def order_food(self, baked_food: BakedFood):
+        self.food_orders.append(baked_food)
+
+    def order_drink(self, drink: Drink):
+        self.drink_orders.append(drink)
+
+    def get_bill(self):
+        bill = 0
+        bill += sum([f.price for f in self.food_orders])
+        bill += sum([d.price for d in self.drink_orders])
+
+        return bill
+
+    def clear(self):
+        self.food_orders.clear()
+        self.drink_orders.clear()
+        self.number_of_people = 0
+        self.is_reserved = False
+
+    def free_table_info(self):
+        if not self.is_reserved:
+            result = f"Table: {self.table_number}\n"
+            result += f"Type: {self.__class__.__name__}\n"
+            result += f"Capacity: {self.capacity}"
+
+            return result
