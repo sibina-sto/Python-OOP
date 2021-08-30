b95026f7b9d8233fcb388b559be1f7d3e8478d84
piziamko <stoyanovasibina@gmail.com>
2021-08-30 22:11:13 +0300

    Add files via upload

---
+++ 0. Python-OOP - Exam/23_Aug_2021/project/planet/planet.py
@@ -0,0 +1,14 @@
+class Planet:
+    def __init__(self, name: str):
+        self.name = name
+        self.items = []
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+        if value == "" or value.isspace():
+            raise ValueError("Planet name cannot be empty string or whitespace!")
+        self.__name = value
