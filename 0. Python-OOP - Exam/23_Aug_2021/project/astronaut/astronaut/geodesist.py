5199f71eb1097531bdf9f384ae99d2062fa5abc9
piziamko <stoyanovasibina@gmail.com>
2021-08-30 22:07:31 +0300

    Delete 0. Python-OOP - Exam/23_Aug_2021/project/astronaut/astronaut directory

--- 0. Python-OOP - Exam/23_Aug_2021/project/astronaut/astronaut/geodesist.py
+++
@@ -1,9 +0,0 @@
-from project.astronaut.astronaut import Astronaut
-
-
-class Geodesist(Astronaut):
-    def __init__(self, name: str):
-        super().__init__(name, 50)
-
-    def breathe(self):
-        self.oxygen -= 10
